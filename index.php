1ef1d63e2161f4c75d6297a12f5bf6cc3af4d3e0
murielledouay <guyard_murielle@orange.fr>
2020-05-07 11:37:56 +0200

    PHP Partie 2 exo 3

--- index.php
+++ index.php
@@ -1,28 +1,24 @@
 <?php
-//déclaration de la variable number avec sa valeur 140 et des variables enchaînant l'addition et la division
-$number = 140;
-$number = $number + 30;
-$number = $number / 2;
+// traduction en else if de l'opérateur ternaire
+$gender = 'femme';
 ?>
-
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
-
 <head>
   <meta charset="utf-8"/>
   <link rel="stylesheet" href="assets/css/style.css"/>
-  <title>Partie 1 exo 6</title>
+  <title>Partie 2 exo 3</title>
 </head>
-
 <body>
-
-  <p>Résultat :
-    <?php
-    //j'affiche avec echo le résultat du calcul ave la variable number
-    echo $number;
-    ?>
-  </p>
-
-
+  <?php
+  if ($gender == 'homme'){
+      echo 'C\'est un développeur!!!';
+    } elseif ($gender == 'femme'){
+      echo 'C\'est une développeuse!!!';
+    }
+    else {
+      echo 'Vous avez fait une erreur dans le genre';
+    }
+  ?>
 </body>
 </html>
